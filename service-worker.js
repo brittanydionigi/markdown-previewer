bc2450b902fba222ded9cd32106ac546afe03e20
Brittany Storoz <brittanystoroz@gmail.com>
2015-03-16 17:26:46 -0400

    Separate examples

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+importScripts('serviceworker-cache-polyfill.js');
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('assets-v15').then(function(cache) {
+      return cache.addAll([
+        'style.css',
+        'markdown-it.js'
+      ]);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+self.addEventListener('message', function(event) {
+  self.clients.matchAll().then(function(client) {
+    client[0].postMessage({
+      message: 'hi there message here!!'
+    });
+  });
+});
